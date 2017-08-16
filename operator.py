3e4ca0f7a939f2fb9b77831ffb01f5abbffad41d
gent95 <mjt950827@163.com>
2017-08-16 23:46:42 +0800

    Initial commit

---
+++ operator.py
@@ -0,0 +1,79 @@
+# a = 1+2
+# print(a)
+#
+# b = "good" + "job"
+# print(b)
+#
+# a = -5
+# print(a)
+# b = -(-9)
+# print(b)
+#
+# c = 9 -2
+# print(c)
+#
+# d = 5 * 9
+# print(d)
+
+# b = "hello" * 6
+# print(b)
+
+#  a = 7 / 2
+# print(a)
+
+# a = 2 ** 3
+# print(a)
+
+# a = 3 < 7 > 9
+# print(a)
+
+# a = 2 != 3
+# print(a)
+
+#取余数部分
+# a = 10 % 3
+# print(a)
+
+# 取整数部分
+# a = 10 // 3
+# print(a)
+
+#1 0为0
+# a = 7 & 18
+# print(a)
+
+#1 0为1
+# a = 7 | 18
+# print(a)
+
+#抑或运算,相同为0,不同为1
+# a = 7 ^ 18
+# print(a)
+
+#~ :按位反转 ~x = -(x +1)
+# a = ~18
+# print(a)
+
+#向左移n位相当于乘以2的n次幂
+# a = 18 << 2
+# print(a)
+
+#向右移n位相当于除以2的n次幂
+# a = 18 >> 2
+# print(a)
+
+#and 其中一个为假则为假
+# print (True and True)
+# print(True and False)
+
+#or其中一个为真则为真
+# print(True or False)
+
+
+
+
+
+
+
+
+
